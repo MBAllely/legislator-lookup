81c5b6f3e2802eef69d1367964c2ba9e2c9cf071
Bri Popson and Marika Allely <mballely@gmail.com>
2016-04-05 08:50:06 -0700

    use functional API key

--- app/routes/results.js
+++ app/routes/results.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=5695a6f476714b8dab430e2841ffd0bf&zip=' + params.zip;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=73e500fb4e1844548aa274ab5b345fcd&zip=' + params.zip;
       return Ember.$.getJSON(url).then(function(responseJSON) {
         return responseJSON.results;
       });

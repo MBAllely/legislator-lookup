51fbe26e26f037b474faea9a15d656e2b6ddbfa3
Bri Popson and Marika Allely <mballely@gmail.com>
2016-04-05 13:28:57 -0700

    add dynamic class application based on legislator party

--- app/routes/results.js
+++ app/routes/results.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    // var isLegislator=(params.routing === "legislators");
     var key = config.myApiKey;
     var url = 'http://congress.api.sunlightfoundation.com/'+ params.routing + '?state='+ params.state + '&apikey=' + key;
       return Ember.$.getJSON(url).then(function(responseJSON) {

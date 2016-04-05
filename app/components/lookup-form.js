09bb67d7827a3f4d6bb639957c090e3dc010617a
Bri Popson and Marika Allely <bpopson@gmail.com>
2016-04-05 16:54:06 -0700

    cleaned up the outputs

--- app/components/lookup-form.js
+++ app/components/lookup-form.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  lookUpId = 'legislator.biomed'
   actions: {
     stateLookup() {
       var params = {
--- app/routes/outputs.js
+++ app/routes/outputs.js
@@ -5,6 +5,7 @@ export default Ember.Route.extend({
   model: function(params) {
     var key = config.myApiKey;
     var stateUrl = 'http://openstates.org/api/v1//legislators/?state='+ params.state + '&active=true&apikey=' + key;
+    console.log(stateUrl);
       return Ember.$.getJSON(stateUrl).then(function(responseJSON) {
         return responseJSON;
       });

ed67dc1ace4807c4ffd233399ecb741220e27609
Bri Popson and Marika Allely <bpopson@gmail.com>
2016-04-05 12:02:30 -0700

    add facebook link to each rep

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     stateLookup(params) {
-      console.log(params);
       this.transitionTo('results', params.routing, params.state);
     }
   }
--- app/routes/results.js
+++ app/routes/results.js
@@ -1,9 +1,11 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/' + params.routing + '?state=' + params.state + '&apikey=73e500fb4e1844548aa274ab5b345fcd';
-    console.log(params);
+    // var isLegislator=(params.routing === "legislators");
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/'+ params.routing + '?state='+ params.state + '&apikey=' + key;
       return Ember.$.getJSON(url).then(function(responseJSON) {
         return responseJSON.results;
       });
